acefdc87de1dcce34bb5ebee19bdc08738ff749c
suicodave <suico.dave@gmail.com>
2021-09-21 21:56:24 +0800

    more config

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,19 +1,18 @@
 const { guessProductionMode } = require("@ngneat/tailwind");
 
 module.exports = {
-    prefix: '',
-    purge: {
-      enabled: guessProductionMode(),
-      content: [
-        './src/**/*.{html,ts}',
-      ]
-    },
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
+  prefix: "",
+  important: true,
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
